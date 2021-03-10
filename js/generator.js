3c21774782e126f0b0dd91ddab2774643a4b2fc7
Jos <140453a@acadiau.ca>
2021-03-10 01:27:54 -0400

    Started jsonifying race+stats

--- js/generator.js
+++ js/generator.js
@@ -1,4 +1,13 @@
+// Checking form data for validity.
+function checkData(data) {
+  if (data.name === '') {
+    console.log("Error: name field is blank.");
+    return false;
+  }
+  return true;
+}
 
+// Retrieving form data and putting in json.
 function getFormData(form){
     var unindexed_array = form.serializeArray();
     var indexed_array = {};
@@ -13,9 +22,12 @@ function getFormData(form){
 function generate() {
   var form = $("#form");
   var data = getFormData(form);
-  var race = $('select[name=race] option').filter(':selected').val()
-  data.race = race
-  var _class = $('select[name=class] option').filter(':selected').val()
-  data.class = _class
+  var err = checkData(data);
+  if (!err) return false;
+  var race = $('select[name=race] option').filter(':selected').val();
+  data.race = race;
+  var _class = $('select[name=class] option').filter(':selected').val();
+  data.class = _class;
   console.log(data);
+  return true;
 }
